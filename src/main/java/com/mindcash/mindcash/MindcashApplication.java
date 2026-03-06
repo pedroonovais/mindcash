b013157aac9c094071842dcdacaf22aeaa37f500
Pedro Novais <phenrique101007@gmail.com>
2026-03-06 20:12:39 -0300

    first commit

---
+++ src/main/java/com/mindcash/mindcash/MindcashApplication.java
@@ -0,0 +1,13 @@
+package com.mindcash.mindcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindcash/mindcash/MindcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindcash.mindcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
